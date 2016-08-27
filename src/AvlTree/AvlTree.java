5f06a63c9817ccace9316de3aac561e4565ddcd7
fisbii <fisher.bi@leandev.se>
2016-08-27 13:49:25 +0800

    AVL Tree.

--- src/AvlTree/AvlTree.java
+++ src/AvlTree/AvlTree.java
@@ -1,10 +1,12 @@
 package AvlTree;
 
 
+import java.util.Comparator;
+
 /**
  * Created by fisbii on 16-8-26.
  */
-public class AvlTree<T> {
+public class AvlTree<T extends Comparable<? super T>> {
 
     private static class AvlNode<T>{
 
@@ -33,7 +35,39 @@ public class AvlTree<T> {
         if(t == null){
             return new AvlNode<T>(x, null, null);
         }
-        
+
+        int compareResult = compare(x,t);
+
+        if(compareResult < 0){
+            t.left = insert(x, t.left);
+            if(height(t.left) - height(t.right) == 2){
+                if(compare(x,t.left) < 0){
+                    t = rotateWithLeftChild(t);
+                }else{
+                    t = doubleWithLeftChild(t);
+                }
+            }else if(height(t.right) - height(t.left) == 2){
+                if(compare(x,t.right) > 0){
+                    t = rotateWithLeftChild(t);
+                }else{
+                    t = doubleWithLeftChild(t);
+                }
+            }
+        }
+        t.height = Math.max(height(t.left),height(t.right)) + 1;
+        return t;
+    }
+
+    private AvlNode<T> doubleWithLeftChild(AvlNode<T> t) {
+        return null;
+    }
+
+    private AvlNode<T> rotateWithLeftChild(AvlNode<T> t) {
+        return null;
+    }
+
+    private int compare(T x, AvlNode<T> t){
+        return x.compareTo(t.element);
     }
 
 }
